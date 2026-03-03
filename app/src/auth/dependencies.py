cb0d6925dae44037639fa110221fe8337d3afbd0
aradhyanand20 <aradhyanand24k@gmail.com>
2026-03-03 23:47:16 +0530

    logout user (revoke access token) using redis

--- app/src/auth/dependencies.py
+++ app/src/auth/dependencies.py
@@ -3,6 +3,7 @@ from fastapi import Request, status
 from fastapi.security.http import HTTPAuthorizationCredentials
 from .utils import decode_token
 from fastapi.exceptions import HTTPException
+from src.db.redis import token_in_blocklist
 
 class TokenBearer(HTTPBearer):
     
@@ -18,8 +19,21 @@ class TokenBearer(HTTPBearer):
         if not self.token_valid(token_data):
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail= "Invalid or expired token"     
+                detail= {
+                    "error": "this token is invalid or expired",
+                    "resolution": "Please get new token"
+                }    
             )
+        if await token_in_blocklist(token_data['jti']):
+            raise HTTPException(
+                status_code= status.HTTP_403_FORBIDDEN,
+                detail={
+                    "error": "this token is invalid or has been revoked",
+                    "resolution": "Please get new token"
+                }
+        
+            )
+
         self.verify_token_data(token_data)
         return token_data
     def verify_token_data(self,token_data):
