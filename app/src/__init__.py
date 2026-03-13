6437e9dd1744f5b19bacf61ed211cfde4aba3010
aradhyanand20 <aradhyanand24k@gmail.com>
2026-03-13 23:45:38 +0530

    middleware: logging, CORS, custom middleware

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -11,20 +11,8 @@ from src.db.models import User
 from src.db.models import Book
 from src.db.models import Review
 from src.db.models import Tags
-from .errors import (
-    create_exception_handler,
-    InvalidCredentials,
-    TagAlreadyExists,
-    TagNotFound,
-    BookNotFound,
-    UserAlreadyExists,
-    InsufficientPermission,
-    AccessTokenRequired,
-    InvalidToken,
-    RefreshTokenRequired,
-    RevokedToken,
-    UserNotFound
-)
+from .errors import register_all_errors
+from .middleware import register_middleware
 
 @asynccontextmanager
 async def life_span(app:FastAPI):
@@ -42,137 +30,15 @@ app = FastAPI(
     swagger_ui_parameters={"persistAuthorization": True}
 )
 
-app.add_exception_handler(
-    UserAlreadyExists,
-    create_exception_handler(
-        status_code= status.HTTP_403_FORBIDDEN,
-        initial_detail={
-            "message":"User with email already exists",
-            "error_code":"user_exists"
-        }
-    )
-)
-
-app.add_exception_handler(
-    UserNotFound,
-    create_exception_handler(
-        status_code=status.HTTP_404_NOT_FOUND,
-        initial_detail={
-            "message":"UserNot Found",
-            "error_code":"user_not_found"
-        }
-    )
-    
-)
-app.add_exception_handler(
-        BookNotFound,
-        create_exception_handler(
-            status_code=status.HTTP_404_NOT_FOUND,
-            initial_detail={
-                "message": "Book not found",
-                "error_code": "book_not_found",
-            },
-        ),
-    )
-
-app.add_exception_handler(
-        InvalidCredentials,
-        create_exception_handler(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            initial_detail={
-                "message": "Invalid Email Or Password",
-                "error_code": "invalid_email_or_password",
-            },
-        ),
-    )
-app.add_exception_handler(
-        InvalidToken,
-        create_exception_handler(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            initial_detail={
-                "message": "Token is invalid Or expired",
-                "resolution": "Please get new token",
-                "error_code": "invalid_token",
-            },
-        ),
-    )
-app.add_exception_handler(
-        RevokedToken,
-        create_exception_handler(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            initial_detail={
-                "message": "Token is invalid or has been revoked",
-                "resolution": "Please get new token",
-                "error_code": "token_revoked",
-            },
-        ),
-    )
-app.add_exception_handler(
-        AccessTokenRequired,
-        create_exception_handler(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            initial_detail={
-                "message": "Please provide a valid access token",
-                "resolution": "Please get an access token",
-                "error_code": "access_token_required",
-            },
-        ),
-    )
-app.add_exception_handler(
-        RefreshTokenRequired,
-        create_exception_handler(
-            status_code=status.HTTP_403_FORBIDDEN,
-            initial_detail={
-                "message": "Please provide a valid refresh token",
-                "resolution": "Please get an refresh token",
-                "error_code": "refresh_token_required",
-            },
-        ),
-    )
-app.add_exception_handler(
-        InsufficientPermission,
-        create_exception_handler(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            initial_detail={
-                "message": "You do not have enough permissions to perform this action",
-                "error_code": "insufficient_permissions",
-            },
-        ),
-    )
-app.add_exception_handler(
-        TagNotFound,
-        create_exception_handler(
-            status_code=status.HTTP_404_NOT_FOUND,
-            initial_detail={"message": "Tag Not Found", "error_code": "tag_not_found"},
-        ),
-    )
-app.add_exception_handler(
-        TagAlreadyExists,
-        create_exception_handler(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            initial_detail={
-                "message": "Tag Already exists",
-                "error_code": "tag_exists",
-            },
-        ),
-    )
-app.add_exception_handler(
-        BookNotFound,
-        create_exception_handler(
-            status_code=status.HTTP_404_NOT_FOUND,
-            initial_detail={
-                "message": "Book Not Found",
-                "error_code": "book_not_found",
-            },
-        ),
-    )
-
 @app.exception_handler(500)
 async def internal_server_error(request,exc):
     return JSONResponse(
         content={"message":"Oops! Somrthing went wrong","error_code":"server_error"},
         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
     )
+        
+register_all_errors(app)
+register_middleware(app)
 
 app.include_router(book_router,prefix=f"/api/{ver}/books", tags=['books'])
 app.include_router(auth_router,prefix=f"/api/{ver}/auth", tags=['auth'])
---
+++ app/src/middleware.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, status
+from fastapi.requests import Request
+import time
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
+def register_middleware(app:FastAPI):
+
+    @app.middleware('http')
+    async def custom_logging(request:Request, call_next):
+        start_time = time.time()
+        print("before", start_time)
+
+        response = await call_next(request)
+        processing_time = time.time() - start_time
+        
+        message  = f"{request.client.host}- {request.method} - {request.url.path} - {response.status_code}- completed after {processing_time}"
+        print(message)
+        return response
+    
+    @app.middleware('http')
+    async def authorization(request: Request, call_next):
+        if not "Authorization" in request.headers:
+            return JSONResponse(
+                content={
+                    "message": "Not Authenticated",
+                    "resolution": "Please provide  the right credentials to prceed"
+                },
+                status_code=status.HTTP_401_UNAUTHORIZED
+            )
+        response = await call_next(request)
+        return response
+    
+    app.add_middleware(
+                       CORSMiddleware,
+                       allow_origins=["*"],
+                       allow_methods=["*"],
+                       alowe_methods=["*"],
+                       allow_headers=["*"],
+                       allow_credentials = True,
+     )
+    
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_host = ["*"]
+    )
