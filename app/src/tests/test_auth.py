c927553201151a204e70cad116f9689937875a4e
aradhyanand20 <aradhyanand24k@gmail.com>
2026-03-30 05:23:06 +0530

    routes tests

---
+++ app/src/tests/test_auth.py
@@ -0,0 +1,17 @@
+auth_prefix = f"/api/v1/auth"
+
+def test_user_creation(fake_user_service,test_client):
+    signup_data ={
+        "username":"khusu12",
+        "email":"khushi@gmail.com",
+        "first_name":"khushi",
+        "last_name":"kumar",
+        "password":"test123"
+
+    }
+    respose = test_client.post(
+        url =f"{auth_prefix}/signup",
+        json=signup_data,
+
+    )
+    assert fake_user_service.user_exists_called_once()
